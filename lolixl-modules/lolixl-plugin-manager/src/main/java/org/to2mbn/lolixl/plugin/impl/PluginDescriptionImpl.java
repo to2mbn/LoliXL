34284c1c4f50226633ed18c16fa16d9432087eca
yushijinhun <yushijinhun@gmail.com>
2016-07-06 18:33:58 +0800

    Add PluginDescriptionResolver

---
+++ lolixl-modules/lolixl-plugin-manager/src/main/java/org/to2mbn/lolixl/plugin/impl/PluginDescriptionImpl.java
@@ -0,0 +1,35 @@
+package org.to2mbn.lolixl.plugin.impl;
+
+import java.io.Serializable;
+import java.util.Set;
+import org.to2mbn.lolixl.plugin.MavenArtifact;
+import org.to2mbn.lolixl.plugin.PluginDescription;
+
+public class PluginDescriptionImpl implements PluginDescription, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private MavenArtifact artifact;
+	private Set<MavenArtifact> dependencies;
+
+	public PluginDescriptionImpl(MavenArtifact artifact, Set<MavenArtifact> dependencies) {
+		this.artifact = artifact;
+		this.dependencies = dependencies;
+	}
+
+	@Override
+	public MavenArtifact getArtifact() {
+		return artifact;
+	}
+
+	@Override
+	public Set<MavenArtifact> getDependencies() {
+		return dependencies;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("PluginDescription [artifact=%s, dependencies=%s]", artifact, dependencies);
+	}
+
+}
---
+++ lolixl-modules/lolixl-plugin-manager/src/main/java/org/to2mbn/lolixl/plugin/impl/PluginDescriptionResolver.java
@@ -0,0 +1,65 @@
+package org.to2mbn.lolixl.plugin.impl;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.to2mbn.lolixl.plugin.MavenArtifact;
+import org.to2mbn.lolixl.plugin.PluginDescription;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class PluginDescriptionResolver {
+
+	private DocumentBuilder documentBuilder;
+	private XPath xpath;
+
+	private XPathExpression xexpGroupId;
+	private XPathExpression xexpArtifactId;
+	private XPathExpression xexpVersion;
+	private XPathExpression xexpDependencies;
+	private XPathExpression xexpPlugin;
+
+	public PluginDescriptionResolver() throws XPathExpressionException, ParserConfigurationException {
+		documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+		// Compile XPaths
+		xpath = XPathFactory.newInstance().newXPath();
+		xexpGroupId = xpath.compile("groupId");
+		xexpArtifactId = xpath.compile("artifactId");
+		xexpVersion = xpath.compile("version");
+		xexpDependencies = xpath.compile("dependencies/dependency");
+		xexpPlugin = xpath.compile("plugin");
+	}
+
+	public MavenArtifact resolveArtifact(Node node) throws XPathExpressionException {
+		return new MavenArtifact(
+				xexpGroupId.evaluate(node),
+				xexpArtifactId.evaluate(node),
+				xexpVersion.evaluate(node));
+	}
+
+	public PluginDescription resolvePlugin(Node node) throws XPathExpressionException {
+		MavenArtifact artifact = resolveArtifact(node);
+		Set<MavenArtifact> dependencies = new LinkedHashSet<>();
+		NodeList nodeList = (NodeList) xexpDependencies.evaluate(node, XPathConstants.NODESET);
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			dependencies.add(resolveArtifact(nodeList.item(i)));
+		}
+		return new PluginDescriptionImpl(artifact, dependencies);
+	}
+
+	public PluginDescription resolve(InputSource source) throws XPathExpressionException, SAXException, IOException {
+		return resolvePlugin((Node) xexpPlugin.evaluate(documentBuilder.parse(source), XPathConstants.NODE));
+	}
+
+}
