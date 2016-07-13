fbafa20105b053c867fd84a82ea26b4a78062ca3
yushijinhun <yushijinhun@gmail.com>
2016-07-13 12:31:03 +0800

    添加DependencyResolverTest

---
+++ lolixl-modules/lolixl-plugin-manager/src/test/java/org/to2mbn/lolixl/plugin/test/DependencyResolverTest.java
@@ -0,0 +1,19 @@
+package org.to2mbn.lolixl.plugin.test;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import org.junit.Test;
+import org.to2mbn.lolixl.plugin.impl.resolver.DependencyResolver;
+import org.to2mbn.lolixl.plugin.maven.MavenArtifact;
+
+public class DependencyResolverTest {
+
+	@Test
+	public void testMerge() {
+		assertEquals(Collections.singleton(new MavenArtifact("org.to2mbn.lolixl", "test-artifact", "2.0")),
+				new DependencyResolver().merge(new HashSet<>(Arrays.asList(new MavenArtifact("org.to2mbn.lolixl", "test-artifact", "2.0"), new MavenArtifact("org.to2mbn.lolixl", "test-artifact", "1.0")))));
+	}
+
+}
