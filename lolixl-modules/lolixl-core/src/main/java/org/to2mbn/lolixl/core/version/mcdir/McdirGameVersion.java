fc9d1b1466636af67a6e10d3161f6e6f4f6955d5
yushijinhun <yushijinhun@gmail.com>
2016-07-17 17:23:24 +0800

    Add org.to2mbn.lolixl.core.version.mcdir

---
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/version/mcdir/McdirGameVersion.java
@@ -0,0 +1,40 @@
+package org.to2mbn.lolixl.core.version.mcdir;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import org.to2mbn.jmccc.option.MinecraftDirectory;
+import org.to2mbn.jmccc.version.Version;
+import org.to2mbn.jmccc.version.parsing.Versions;
+import org.to2mbn.lolixl.core.version.GameVersion;
+
+public class McdirGameVersion implements GameVersion {
+
+	private String version;
+	private Path mcdir;
+
+	public McdirGameVersion(String name, Path mcdir) {
+		this.version = name;
+		this.mcdir = mcdir;
+	}
+
+	@Override
+	public String getName() {
+		return version;
+	}
+
+	@Override
+	public Path getMinecraftDirectory() {
+		return mcdir;
+	}
+
+	@Override
+	public Version getLaunchableVersion() {
+		try {
+			return Versions.resolveVersion(new MinecraftDirectory(mcdir.toFile()), version);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+}
---
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/version/mcdir/McdirGameVersionProvider.java
@@ -0,0 +1,39 @@
+package org.to2mbn.lolixl.core.version.mcdir;
+
+import static java.util.stream.Collectors.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+import org.to2mbn.jmccc.option.MinecraftDirectory;
+import org.to2mbn.jmccc.version.parsing.Versions;
+import org.to2mbn.lolixl.core.version.GameVersion;
+import org.to2mbn.lolixl.core.version.GameVersionProvider;
+import org.to2mbn.lolixl.i18n.I18N;
+
+public class McdirGameVersionProvider implements GameVersionProvider {
+
+	private Path location;
+
+	public McdirGameVersionProvider(Path location) {
+		this.location = location;
+	}
+
+	@Override
+	public String getLocalizedName() {
+		if (location.toAbsolutePath().equals(new File(".minecraft").toPath().toAbsolutePath())) {
+			return I18N.localize("org.to2mbn.lolixl.core.version.mcdir.default.name");
+		}
+		return location.toString();
+	}
+
+	@Override
+	public Map<String, GameVersion> listVersions() {
+		return Versions.getVersions(new MinecraftDirectory(location.toFile())).stream()
+				.collect(toMap(ver -> ver, ver -> new McdirGameVersion(ver, location)));
+	}
+
+	public String getLocationProperty() {
+		return "mcdir:" + location;
+	}
+
+}
