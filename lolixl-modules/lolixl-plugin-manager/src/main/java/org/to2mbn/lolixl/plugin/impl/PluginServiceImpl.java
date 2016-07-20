69563e26e5ac4fa342683b6bcaded0c5ddb72f9c
yushijinhun <yushijinhun@gmail.com>
2016-07-20 18:58:37 +0800

    在进行插件安装后refresh

--- lolixl-modules/lolixl-plugin-manager/src/main/java/org/to2mbn/lolixl/plugin/impl/PluginServiceImpl.java
+++ lolixl-modules/lolixl-plugin-manager/src/main/java/org/to2mbn/lolixl/plugin/impl/PluginServiceImpl.java
@@ -27,6 +27,7 @@ import org.apache.felix.scr.annotations.Service;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.wiring.FrameworkWiring;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.event.EventAdmin;
 import org.to2mbn.lolixl.plugin.DependencyAction;
@@ -427,6 +428,8 @@ public class PluginServiceImpl implements PluginService {
 					checkDependenciesState();
 				}
 			} finally {
+				bundleContext.getBundle(0).adapt(FrameworkWiring.class).refreshBundles(null);
+
 				for (Bundle bundle : bundle2artifact.keySet()) {
 					if (bundle.getState() != Bundle.ACTIVE &&
 							bundle.getState() != Bundle.STARTING) {
