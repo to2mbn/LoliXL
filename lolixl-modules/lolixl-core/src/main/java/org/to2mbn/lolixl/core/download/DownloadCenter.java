4c3c7c4463284f674d7046ee439ccee9f29f0dfa
yushijinhun <yushijinhun@gmail.com>
2016-07-21 12:42:07 +0800

    添加DownloadCenter，管理下载任务

---
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/download/DownloadCenter.java
@@ -0,0 +1,13 @@
+package org.to2mbn.lolixl.core.download;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+import org.to2mbn.jmccc.mcdownloader.MinecraftDownloader;
+import org.to2mbn.lolixl.core.ui.DisplayableItem;
+
+public interface DownloadCenter {
+
+	<T> CompletableFuture<T> startTask(DisplayableItem itemToDisplay, Function<MinecraftDownloader, Future<T>> operation);
+
+}
