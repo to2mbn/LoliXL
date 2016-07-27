7974762b6f8cbc0b94d3db295141cae27d231055
darkyoooooo <2569766005@qq.com>
2016-07-28 00:33:16 +0800

    实现窗体可拖动

--- lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/DefaultTitleBarPresenter.java
+++ lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/DefaultTitleBarPresenter.java
@@ -9,11 +9,13 @@ import org.to2mbn.lolixl.ui.impl.container.view.DefaultTitleBarView;
 import java.util.function.Consumer;
 
 public class DefaultTitleBarPresenter extends Presenter<DefaultTitleBarView> {
-
 	private static final String LOCATION_OF_FXML = "/ui/fxml/container/default_title_bar.fxml";
 
 	private Consumer<MouseEvent> closeButtonListener;
 	private Stage parentStage;
+
+	// for draggable:
+	private double lastX, lastY;
 
 	@Override
 	public void postInitialize() {
@@ -26,28 +28,36 @@ public class DefaultTitleBarPresenter extends Presenter<DefaultTitleBarView> {
 		makeDraggable();
 	}
 
+	public void setCloseButtonListener(Consumer<MouseEvent> _closeButtonListener) {
+		closeButtonListener = _closeButtonListener;
+	}
+
+	public void setParentStage(Stage _parentStage) {
+		parentStage = _parentStage;
+	}
+
+	@Override
+	protected String getFxmlLocation() {
+		return LOCATION_OF_FXML;
+	}
+
+
+	private void makeDraggable() {
+		view.rootContainer.setOnMousePressed(event -> {
+			lastX = event.getSceneX();
+			lastY = event.getSceneY();
+		});
+		view.rootContainer.setOnMouseDragged(event -> {
+			parentStage.setX(event.getScreenX() - lastX);
+			parentStage.setY(event.getScreenY() - lastY);
+		});
+	}
+
 	private void onCloseButtonClicked(MouseEvent event) {
 		closeButtonListener.accept(event);
 	}
 
 	private void onMinimizeButtonClicked(MouseEvent event) {
 		parentStage.hide();
-	}
-
-	private void makeDraggable() {
-		// TODO
-	}
-
-	public void setCloseButtonListener(Consumer<MouseEvent> closeButtonListener) {
-		this.closeButtonListener = closeButtonListener;
-	}
-
-	public void setParentStage(Stage parentStage) {
-		this.parentStage = parentStage;
-	}
-
-	@Override
-	protected String getFxmlLocation() {
-		return LOCATION_OF_FXML;
 	}
 }
