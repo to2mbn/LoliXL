ec9540e00b8ed028b78b01207a203ece3fe31d84
Darkyoooooo <2569766005@qq.com>
2016-08-03 16:04:08 +0800

    完善磁贴

--- lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/HomeContentPresenter.java
+++ lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/HomeContentPresenter.java
@@ -2,17 +2,29 @@ package org.to2mbn.lolixl.ui.impl.container.presenter;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.component.ComponentContext;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+import org.to2mbn.lolixl.core.config.ConfigurationEvent;
+import org.to2mbn.lolixl.ui.SideBarTileService;
 import org.to2mbn.lolixl.ui.component.Tile;
 import org.to2mbn.lolixl.ui.container.presenter.Presenter;
 import org.to2mbn.lolixl.ui.impl.container.view.HomeContentView;
 
-@Service({ HomeContentPresenter.class })
+@Service({ HomeContentPresenter.class, EventHandler.class })
+@Properties({ @Property(name = EventConstants.EVENT_TOPIC, value = ConfigurationEvent.TOPIC_CONFIGURATION), @Property(name = EventConstants.EVENT_FILTER, value = "(" + ConfigurationEvent.KEY_CATEGORY + "=" + SideBarTileService.CATEGORY_SIDEBAR_TILES + ")") })
 @Component(immediate = true)
-public class HomeContentPresenter extends Presenter<HomeContentView> {
+public class HomeContentPresenter extends Presenter<HomeContentView> implements EventHandler {
 
 	private static final String FXML_LOCATION = "/ui/fxml/container/home_content.fxml";
+
+	@Reference
+	private SideBarTileService tileService;
 
 	@Activate
 	public void active(ComponentContext compCtx) {
@@ -26,11 +38,20 @@ public class HomeContentPresenter extends Presenter<HomeContentView> {
 
 	/**
 	 * Lazy-bind for tileManagementTile.
-	 * 
+	 *
 	 * @param tile
 	 */
 	public void setManagementTile(Tile tile) {
 		view.tileRootContainer.setBottom(tile);
 	}
 
+
+	@Override
+	public void handleEvent(Event event) {
+		refreshAnimatedTiles();
+	}
+
+	private void refreshAnimatedTiles() {
+		// TODO
+	}
 }
--- lolixl-modules/lolixl-ui/src/main/java/org/to2mbn/lolixl/ui/component/DisplayableItemTile.java
+++ lolixl-modules/lolixl-ui/src/main/java/org/to2mbn/lolixl/ui/component/DisplayableItemTile.java
@@ -1,6 +1,9 @@
 package org.to2mbn.lolixl.ui.component;
 
+import org.to2mbn.lolixl.ui.component.view.DisplayableItemTileView;
 import org.to2mbn.lolixl.ui.model.DisplayableItem;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 /**
  * 从 {@link DisplayableItem} 创建的磁贴。
@@ -15,9 +18,16 @@ import org.to2mbn.lolixl.ui.model.DisplayableItem;
 public class DisplayableItemTile extends Tile {
 
 	private DisplayableItem item;
+	private DisplayableItemTileView graphic;
 
 	public DisplayableItemTile(DisplayableItem item) {
 		this.item = item;
+		try {
+			graphic = new DisplayableItemTileView();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		setGraphic(graphic);
 	}
 
 	// TODO
