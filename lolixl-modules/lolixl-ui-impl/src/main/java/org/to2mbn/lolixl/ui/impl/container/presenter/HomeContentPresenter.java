41d862e3fb48adddbe2daefe543aecbe80627274
darkyoooooo <2569766005@qq.com>
2016-08-03 19:41:56 +0800

    没写完

--- lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/HomeContentPresenter.java
+++ lolixl-modules/lolixl-ui-impl/src/main/java/org/to2mbn/lolixl/ui/impl/container/presenter/HomeContentPresenter.java
@@ -1,5 +1,10 @@
 package org.to2mbn.lolixl.ui.impl.container.presenter;
 
+import javafx.animation.Transition;
+import javafx.animation.TranslateTransition;
+import javafx.collections.ListChangeListener;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
@@ -15,6 +20,9 @@ import org.to2mbn.lolixl.ui.SideBarTileService;
 import org.to2mbn.lolixl.ui.component.Tile;
 import org.to2mbn.lolixl.ui.container.presenter.Presenter;
 import org.to2mbn.lolixl.ui.impl.container.view.HomeContentView;
+import org.to2mbn.lolixl.ui.model.SidebarTileElement;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service({ HomeContentPresenter.class, EventHandler.class })
 @Properties({
@@ -50,10 +58,45 @@ public class HomeContentPresenter extends Presenter<HomeContentView> implements 
 
 	@Override
 	public void handleEvent(Event event) {
-		refreshAnimatedTiles();
+		refreshShownTiles();
 	}
 
-	private void refreshAnimatedTiles() {
-		// TODO
+	private void refreshShownTiles() {
+		// FIXME: 简直日了狗 在idea下用lambda会报错
+		tileService.getTiles(SideBarTileService.StackingStatus.SHOWN).addListener(new ListChangeListener<SidebarTileElement>() {
+			@Override
+			public void onChanged(Change<? extends SidebarTileElement> change) {
+				change.getAddedSubList().forEach(ele -> {
+					Tile tile = ele.createTile();
+					tile.addEventHandler(MouseEvent.MOUSE_MOVED, event -> {
+					});
+					tile.addEventHandler(MouseEvent.MOUSE_EXITED, event -> {
+					});
+				});
+			}
+		});
+	}
+
+	private static class SideBarTileAnimationHandler {
+		private final Tile tile;
+		private final AtomicBoolean isPlaying = new AtomicBoolean(false);
+		private final AtomicReference<Transition> currentAnimation = new AtomicReference<>(null);
+
+		private SideBarTileAnimationHandler(Tile _tile) {
+			tile = _tile;
+		}
+
+		private void runRollOutAnimation() {
+			TranslateTransition tran = new TranslateTransition(Duration.millis(800), tile);
+			double x = tile.getLayoutX();
+			tran.setFromX(x + tile.getWidth());
+			tran.setToX(x);
+			tran.setOnFinished(event -> isPlaying.set(false));
+			tran.play();
+		}
+
+		private void cancelAndFallback() {
+			// TODO
+		}
 	}
 }
