3a958ff0838bf277da62a6f99250e64bc7d30fd9
Darkyoooooo <2569766005@qq.com>
2016-08-08 00:34:29 +0800

    Revert "给yushi挖坑"
    This reverts commit b0c3c5bc297091392c5a83e7a2928d47aa7f0bd3.

--- lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/VersionTagResolver.java
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/VersionTagResolver.java
@@ -3,6 +3,6 @@ package org.to2mbn.lolixl.core.game.version;
 import org.to2mbn.jmccc.version.Version;
 import java.util.Set;
 
-public interface VersionTagResolutionService {
+public interface VersionTagResolver {
 	Set<String> resolveTags(Version version);
 }
