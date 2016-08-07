b0c3c5bc297091392c5a83e7a2928d47aa7f0bd3
Darkyoooooo <2569766005@qq.com>
2016-08-08 00:12:36 +0800

    给yushi挖坑

--- lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/VersionTagResolutionService.java
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/VersionTagResolutionService.java
@@ -3,6 +3,6 @@ package org.to2mbn.lolixl.core.game.version;
 import org.to2mbn.jmccc.version.Version;
 import java.util.Set;
 
-public interface VersionTagResolver {
+public interface VersionTagResolutionService {
 	Set<String> resolveTags(Version version);
 }
