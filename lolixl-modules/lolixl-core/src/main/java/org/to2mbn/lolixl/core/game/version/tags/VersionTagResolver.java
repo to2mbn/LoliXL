bbc5081046a32ce9c2d7b5f4b10a1e93627492ce
yushijinhun <yushijinhun@gmail.com>
2016-08-08 22:20:48 +0800

    VersionTagResolver支持databind

--- lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/tags/VersionTagResolver.java
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/version/tags/VersionTagResolver.java
@@ -1,9 +1,9 @@
 package org.to2mbn.lolixl.core.game.version.tags;
 
 import org.to2mbn.jmccc.version.Version;
-import java.util.Set;
+import javafx.collections.ObservableSet;
 
 public interface VersionTagResolver {
 
-	Set<VersionTag> resolveTags(Version version);
+	ObservableSet<VersionTag> resolveTags(Version version);
 }
