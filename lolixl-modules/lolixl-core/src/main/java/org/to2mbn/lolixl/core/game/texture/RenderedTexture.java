0cb106cfbf8fa4fce344187ae6531cda330a67a5
yushijinhun <yushijinhun@gmail.com>
2016-08-09 11:52:40 +0800

    完善RenderedTexture api

--- lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/texture/RenderedTexture.java
+++ lolixl-modules/lolixl-core/src/main/java/org/to2mbn/lolixl/core/game/texture/RenderedTexture.java
@@ -1,14 +1,30 @@
 package org.to2mbn.lolixl.core.game.texture;
 
-import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
 
 public interface RenderedTexture {
 
 	// ==== 2D apis ====
 
-	Image getAvatar();
+	/**
+	 * Renders the avatar to the given {@link GraphicsContext}.
+	 * <p>
+	 * The {@code height} is computed by {@code width}.
+	 * 
+	 * @param ctx
+	 * @param width
+	 */
+	void renderAvatar(GraphicsContext ctx, double width);
 
-	Image getFullBodyShot();
+	/**
+	 * Renders the full-body shot to the given {@link GraphicsContext}.
+	 * <p>
+	 * The {@code height} is computed by {@code width}.
+	 * 
+	 * @param ctx
+	 * @param width
+	 */
+	void renderFullBodyShot(GraphicsContext ctx, double width);
 
 	// ==== 3D apis ====
 	// TODO: Add 3d apis for RenderedTexture
